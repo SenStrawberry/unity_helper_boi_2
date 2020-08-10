4e5988a67b78d14171516ddec045284ed64ec2aa
SenStrawberry <senstrawberry@gmail.com>
2020-08-10 15:53:11 +0100

    initial commit

---
+++ Assets/input_and_processing.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class input_and_processing : MonoBehaviour
+{
+    public string user_input;
+    public GameObject input_field;
+    public GameObject text_display;
+    public List<string> Progams;
+    public string[] workspace_list = {"Blender", "Anki", "Chrome"};
+    
+
+
+    public void store_user_input()
+    {
+        user_input = input_field.GetComponent<Text>().text;
+        text_display.GetComponent<Text>().text = "user said: " + user_input;
+    }
+
+    public void open_command()
+    {
+        if(user_input == "open")
+        {
+         
+            Process.Start("C:\\Program Files\\Blender Foundation\\Blender 2.82\\blender.exe");
+
+        }
+    }
+    
+    public void open_workspaces()
+    {
+        foreach (string i in workspace_list)
+        {
+            Process.Start("C:\\Program Files\\Blender Foundation\\Blender 2.82\\blender.exe");
+        }
+    }
+
+
+    public void workspace_opening()
+    {   foreach (string i in workspace)
+        {
+            bool program_found = false;
+            while (program_found == false)
+            {
+                foreach (string x in progam_list)
+                {
+                    if (program_list[0, x] == workspace[i])
+                    {
+                        Process.Start(program_list[1, x]);
+                        bool program_found = true;
+                    }
+                }
+            }
+        }
+    }
+}
